f95de4c9e20d9825d8c2e461bd3a1a0bc0d3e526
alvics <alvics71@hotmail.com>
2018-12-30 18:49:40 +1000

    nested call backs

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,8 @@ import SearchBar from './SearchBar';
 import Youtube from '../apis/Youtube';
 import VideoList from './VideoList';
 
-
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
     const response = await Youtube.get('/search', {
@@ -16,11 +15,18 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = video => {
+    console.log('From the App, video is selected', video);
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
